7f88b5badcdad7160f89cf8dd380a9a4b3f7d7d6
Rohit Deokar <rdeokar096@gmail.com>
2026-05-24 00:19:07 +0530

    port fixed

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const connectDB = require('./config/db');
 // Connect Database
 connectDB();
 
-const PORT = process.env.FRONTEND_URL || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
