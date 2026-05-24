b0b1e03fd83bb3785d2c5feb5d0a6ad49eddce70
Rohit126tech <rdeokar096@gmail.com>
2026-05-24 13:06:37 +0530

    Add PATCH method to CORS configuration

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(
       "http://localhost:4200"
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
