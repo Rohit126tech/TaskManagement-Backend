3d9369c1cfabbaf927cd640bba4655eeae61cf90
Rohit Deokar <rdeokar096@gmail.com>
2026-05-24 19:50:55 +0530

    sync changes

--- src/app.js
+++ src/app.js
@@ -12,13 +12,13 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 // CORS
-
 app.use(
   cors({
     origin: [
       "http://localhost:4200",
       "https://task-management-fontend-4nzj.vercel.app",
       "https://task-management-fontend-4nzj-o5xh8pf3k-rohit-deokar-s-projects.vercel.app",
+      "https://task-management-fontend-4nzj-b75i8yk10-rohit-deokar-s-projects.vercel.app",
     ],
     credentials: true,
   }),
